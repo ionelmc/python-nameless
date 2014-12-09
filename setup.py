d25f2cd87c5e8d56808244c86a928fdfbab21f57
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-12-09 02:12:09 +0200

    Update from template.

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
 # -*- encoding: utf-8 -*-
-import glob
 import io
+import os
 import re
+from glob import glob
 from os.path import basename
 from os.path import dirname
 from os.path import join
+from os.path import relpath
 from os.path import splitext
 
 from setuptools import find_packages
 from setuptools import setup
-
 
 def read(*names, **kwargs):
     return io.open(
         join(dirname(__file__), *names),
         encoding=kwargs.get("encoding", "utf8")
     ).read()
+
+
+
+
 
 setup(
     name="nameless",
@@ -28,7 +33,7 @@ setup(
     url="https://github.com/ionelmc/python-nameless",
     packages=find_packages("src"),
     package_dir={"": "src"},
-    py_modules=[splitext(basename(i))[0] for i in glob.glob("src/*.py")],
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
@@ -56,12 +61,11 @@ setup(
         # eg: "aspectlib==1.1.1", "six>=1.7",
     ],
     extras_require={
-        # eg: 'rst': ["docutils>=0.11"],
+        # eg: "rst": ["docutils>=0.11"],
     },
     entry_points={
         "console_scripts": [
             "nameless = nameless.__main__:main"
         ]
-    }
-
+    },
 )
