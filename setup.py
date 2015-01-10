6d640731ecfedb5a6e9bb416d0ee13f4d8964157
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-01-10 22:28:20 +0200

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(*names, **kwargs):
 
 setup(
     name="nameless",
-    version="0.1.1",
+    version="0.1.2",
     license="BSD",
     description="An example package. Replace this with a proper project description. Generated with https://github.com/ionelmc/cookiecutter-pylibrary",
     long_description="%s\n%s" % (read("README.rst"), re.sub(":obj:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst"))),
