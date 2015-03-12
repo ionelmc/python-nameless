ca740c543d6029f794f3cca50c54aa5eee0eb2f2
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-03-12 13:01:05 +0200

    Update from template.

--- setup.py
+++ setup.py
@@ -18,54 +18,54 @@ from setuptools import setup
 def read(*names, **kwargs):
     return io.open(
         join(dirname(__file__), *names),
-        encoding=kwargs.get("encoding", "utf8")
+        encoding=kwargs.get('encoding', 'utf8')
     ).read()
 
 
 setup(
-    name="nameless",
-    version="0.1.0",
-    license="BSD",
-    description="An example package. Replace this with a proper project description. Generated with https://github.com/ionelmc/cookiecutter-pylibrary",
-    long_description="{0}\n{1}".format(read("README.rst"), re.sub(":obj:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst"))),
-    author="Ionel Cristian Maries",
-    author_email="contact@ionelmc.ro",
-    url="https://github.com/ionelmc/python-nameless",
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
+    name='nameless',
+    version='0.1.0',
+    license='BSD',
+    description='An example package. Replace this with a proper project description. Generated with https://github.com/ionelmc/cookiecutter-pylibrary',
+    long_description='%s\n%s' % (read('README.rst'), re.sub(':obj:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))),
+    author='Ionel Cristian Maries',
+    author_email='contact@ionelmc.ro',
+    url='https://github.com/ionelmc/python-nameless',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: Unix",
-        "Operating System :: POSIX",
-        "Operating System :: Microsoft :: Windows",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Utilities",
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Utilities',
     ],
     keywords=[
-        # eg: "keyword1", "keyword2", "keyword3",
+        # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
     install_requires=[
-        # eg: "aspectlib==1.1.1", "six>=1.7",
+        # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
     extras_require={
-        # eg: "rst": ["docutils>=0.11"],
+        # eg: 'rst': ['docutils>=0.11'],
     },
     entry_points={
-        "console_scripts": [
-            "nameless = nameless.__main__:main"
+        'console_scripts': [
+            'nameless = nameless.__main__:main'
         ]
     },
 )
