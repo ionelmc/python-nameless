e0bf3ab967ae48661cfb2130bab0a7f7fd9f01dc
Ionel Cristian Mărieș <contact@ionelmc.ro>
2019-01-10 09:14:18 +0200

    Up.

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setup(
         # 'Programming Language :: Python :: Implementation :: Stackless',
         'Topic :: Utilities',
     ],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
