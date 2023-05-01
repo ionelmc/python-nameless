2a0c92b9ad9a6127eb32eefa501cebcc6b681cb7
Ionel Cristian Mărieș <contact@ionelmc.ro>
2023-05-02 02:11:02 +0300

    Up.

--- setup.py
+++ setup.py
@@ -19,12 +19,12 @@ except ImportError:
 # Enable code coverage for C code: we cannot use CFLAGS=-coverage in tox.ini, since that may mess with compiling
 # dependencies (e.g. numpy). Therefore we set SETUPPY_CFLAGS=-coverage in tox.ini and copy it to CFLAGS here (after
 # deps have been safely installed).
-if 'TOX_ENV_NAME' in os.environ and os.environ.get('SETUPPY_EXT_COVERAGE') == 'yes':
-    CFLAGS = os.environ['CFLAGS'] = '-DCYTHON_TRACE=1'
-    LFLAGS = os.environ['LFLAGS'] = ''
+if "TOX_ENV_NAME" in os.environ and os.environ.get("SETUPPY_EXT_COVERAGE") == "yes":
+    CFLAGS = os.environ["CFLAGS"] = "-DCYTHON_TRACE=1"
+    LFLAGS = os.environ["LFLAGS"] = ""
 else:
-    CFLAGS = ''
-    LFLAGS = ''
+    CFLAGS = ""
+    LFLAGS = ""
 
 
 class OptionalBuildExt(build_ext):
@@ -34,15 +34,15 @@ class OptionalBuildExt(build_ext):
 
     def run(self):
         try:
-            if os.environ.get('SETUPPY_FORCE_PURE'):
-                raise Exception('C extensions disabled (SETUPPY_FORCE_PURE)!')
+            if os.environ.get("SETUPPY_FORCE_PURE"):
+                raise Exception("C extensions disabled (SETUPPY_FORCE_PURE)!")
             super().run()
         except Exception as e:
             self._unavailable(e)
             self.extensions = []  # avoid copying missing files (it would fail).
 
     def _unavailable(self, e):
-        print('*' * 80)
+        print("*" * 80)
         print(
             """WARNING:
 
@@ -52,10 +52,10 @@ class OptionalBuildExt(build_ext):
             """
         )
 
-        print('CAUSE:')
-        print('')
-        print('    ' + repr(e))
-        print('*' * 80)
+        print("CAUSE:")
+        print("")
+        print("    " + repr(e))
+        print("*" * 80)
 
 
 class BinaryDistribution(Distribution):
@@ -64,68 +64,68 @@ class BinaryDistribution(Distribution):
     """
 
     def has_ext_modules(self):
-        return super().has_ext_modules() or not os.environ.get('SETUPPY_ALLOW_PURE')
+        return super().has_ext_modules() or not os.environ.get("SETUPPY_ALLOW_PURE")
 
 
 def read(*names, **kwargs):
-    with Path(__file__).parent.joinpath(*names).open(encoding=kwargs.get('encoding', 'utf8')) as fh:
+    with Path(__file__).parent.joinpath(*names).open(encoding=kwargs.get("encoding", "utf8")) as fh:
         return fh.read()
 
 
 setup(
-    name='nameless',
+    name="nameless",
     use_scm_version={
-        'local_scheme': 'dirty-tag',
-        'write_to': 'src/nameless/_version.py',
-        'fallback_version': '1.0.0',
+        "local_scheme": "dirty-tag",
+        "write_to": "src/nameless/_version.py",
+        "fallback_version": "0.1.0",
     },
-    license='BSD-2-Clause',
-    description='An example package. Generated with cookiecutter-pylibrary.',
-    long_description='{}\n{}'.format(
-        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
-        re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst')),
+    license="BSD-2-Clause",
+    description="An example package. Generated with cookiecutter-pylibrary.",
+    long_description="{}\n{}".format(
+        re.compile("^.. start-badges.*^.. end-badges", re.M | re.S).sub("", read("README.rst")),
+        re.sub(":[a-z]+:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst")),
     ),
-    author='Ionel Cristian Mărieș',
-    author_email='contact@ionelmc.ro',
-    url='https://github.com/ionelmc/python-nameless',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[path.stem for path in Path('src').glob('*.py')],
+    author="Ionel Cristian Mărieș",
+    author_email="contact@ionelmc.ro",
+    url="https://github.com/ionelmc/python-nameless",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[path.stem for path in Path("src").glob("*.py")],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
         # uncomment if you test on these interpreters:
         # "Programming Language :: Python :: Implementation :: IronPython",
         # "Programming Language :: Python :: Implementation :: Jython",
         # "Programming Language :: Python :: Implementation :: Stackless",
-        'Topic :: Utilities',
+        "Topic :: Utilities",
     ],
     project_urls={
-        'Documentation': 'https://python-nameless.readthedocs.io/',
-        'Changelog': 'https://python-nameless.readthedocs.io/en/latest/changelog.html',
-        'Issue Tracker': 'https://github.com/ionelmc/python-nameless/issues',
+        "Documentation": "https://python-nameless.readthedocs.io/",
+        "Changelog": "https://python-nameless.readthedocs.io/en/latest/changelog.html",
+        "Issue Tracker": "https://github.com/ionelmc/python-nameless/issues",
     },
     keywords=[
         # eg: "keyword1", "keyword2", "keyword3",
     ],
-    python_requires='>=3.7',
+    python_requires=">=3.7",
     install_requires=[
         # eg: "aspectlib==1.1.1", "six>=1.7",
     ],
@@ -135,28 +135,28 @@ setup(
         #   ":python_version=="2.6"": ["argparse"],
     },
     setup_requires=[
-        'setuptools_scm>=3.3.1',
-        'cython',
+        "setuptools_scm>=3.3.1",
+        "cython",
     ]
     if Cython
     else [
-        'setuptools_scm>=3.3.1',
+        "setuptools_scm>=3.3.1",
     ],
     entry_points={
-        'console_scripts': [
-            'nameless = nameless.cli:main',
+        "console_scripts": [
+            "nameless = nameless.cli:main",
         ]
     },
-    cmdclass={'build_ext': OptionalBuildExt},
+    cmdclass={"build_ext": OptionalBuildExt},
     ext_modules=[
         Extension(
-            str(path.relative_to('src').with_suffix('')).replace(os.sep, '.'),
+            str(path.relative_to("src").with_suffix("")).replace(os.sep, "."),
             sources=[str(path)],
             extra_compile_args=CFLAGS.split(),
             extra_link_args=LFLAGS.split(),
             include_dirs=[str(path.parent)],
         )
-        for path in Path('src').glob('**/*.pyx' if Cython else '**/*.c')
+        for path in Path("src").glob("**/*.pyx" if Cython else "**/*.c")
     ],
     distclass=BinaryDistribution,
 )
